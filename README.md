0533070128347859ef4474704947181d0c2a6762
gitadmin <gitadmin@muenai.com>
2020-05-22 16:33:47 +0800

    init

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # vue-rxjs
 
+
+## learning note
+- vue router 需要去補一下
+- rxjs 除了觀念要對外，也需要懂如何善用operator 才可以發揮其強大的地方...
+- 基本上，declarative functional 程式，就是要很懂那些操作子之間的利用，才可以取代回歸到imperative 程式了。
+
+## rxjs example page
+https://github.com/vuejs/vue-rx/blob/master/example/wiki-search.html
+
+
 ## Project setup
 ```
 npm install
--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Type to search Wikipedia</p>
+    <input v-model="search" />
+    <div v-if="results">
+      <h1>Wiki search result for : {{ results.term }}</h1>
+      <ul style="list-style: none;" v-if="results.matches.length">
+        <li v-for="match in results.matches" :key="match.url">
+          <a :href="match.url">{{ match.title }}</a>
+          <p>{{ match.description }}</p>
+        </li>
+      </ul>
+      <p v-else> No matches found.</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import { ajax } from "rxjs/ajax";
+import { pluck, filter, debounceTime, distinctUntilChanged, switchMap, map } from "rxjs/operators";
+
+const baseUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&origin=*";
+
+// 利用rxjs內建的ajax去拿api資料
+function fetchTerm(searchTerm) {
+  return ajax.getJSON(`${baseUrl}&search=${searchTerm}`);
+}
+
+// 拿回的資料返回一個object
+function formatResult(res) {
+  return {
+    term: res[0],
+    matches: res[1].map((title, i) => ({
+      title: title,
+      description: res[2][i],
+      url: res[3][i]
+    }))
+  };
+}
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+  data() {
+    return {
+      search: ""
+    };
+  },
+  subscriptions() {
+    return {
+      // $watchAsObservable 會根据一个值的侦听器创建 observable。值会以 { newValue, oldValue } 的格式触发
+      results: this.$watchAsObservable("search").pipe(
+        pluck("newValue"), // 选择属性来发出，這邊是拿到newValue才發出
+        filter(text => text.length > 2),  // 要大於2
+        debounceTime(300),  // 至少輸入後0.3 s
+        distinctUntilChanged(), // 只有當拿到的值跟上一個值不同才發出
+        switchMap(fetchTerm), // 完成前一个内部 observable，发出值
+        map(formatResult) // 对源 observable 的每个值应用投射函数
+      )
+    };
   }
-}
+};
 </script>
 
 <style>
@@ -25,4 +74,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
